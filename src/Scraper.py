0ceca7bf2239fa81e345f22f5f691612046a704a
Parth Patel <parthpatel@parths-mbp.myfiosgateway.com>
2021-05-22 14:15:41 -0400

    Fix formatting

--- src/Scraper.py
+++ src/Scraper.py
@@ -38,6 +38,7 @@ class Scraper :
         """
         Takes in url and returns list of comments or Error
         """
+        
         url = self.__getCurrentDiscussionThreadURL()
         if url == "ERROR":
             return url
