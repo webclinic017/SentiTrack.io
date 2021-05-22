188c7a99a003c50da3f9079e61f45901a5871c96
Parth Patel <parthpatel@parths-mbp.myfiosgateway.com>
2021-05-22 14:09:26 -0400

    Add method to fetch active discussion endpoint

--- src/Scraper.py
+++ src/Scraper.py
@@ -1,11 +1,29 @@
 import requests
 
 class Scraper :
-    
+
+    def __getCurrentDiscussionThreadURL(self):
+
+        """
+        Fetches endpoint of current/active discussion thread
+        """
+
+        base_url = "https://www.reddit.com"
+        front_page = "https://www.reddit.com/r/wallstreetbets/.json"
+        req_data = requests.get(front_page, headers={'User-agent': 'rtst.com'})
+        if req_data.status_code != 200:
+            return "ERROR"
+        #active discussion is always pinned at top
+        json_response = req_data.json()
+        url = base_url + json_response['data']['children'][0]['data']['permalink'] + ".json"
+        return url
+
     def __parseChildren(self, json_response):
+
         """
         Parses json response and returns list of comments
         """
+
         comments = []
         children = json_response[1]['data']['children']
         for child in children:
@@ -16,13 +34,14 @@ class Scraper :
         return comments
     
     def getRequestResponse(self, url):
+
         """
         Takes in url and returns list of comments or Error
         """
+
         req_data = requests.get(url, headers={'User-agent': 'rtst.com'})
         if req_data.status_code == 200:
             json_data = req_data.json()
             return self.__parseChildren(json_data)
         else:
-            return "Error"
-    
+            return "ERROR"
