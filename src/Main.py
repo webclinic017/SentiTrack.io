6c78888cbc9963f6ed48dc6a26794b7bda13fdef
Parth Patel <parthpatel@parths-mbp.myfiosgateway.com>
2021-06-12 16:08:03 -0400

    Add main

--- src/Main.py
+++ src/Main.py
@@ -1,10 +1,13 @@
-import UI
-import Scraper, Classifier
+import UI, Scraper, Classifier
 from CustomTransformer import cleanText, TextCleaner
-
-def main():
-    ui = UI.UI()
-    ui.textUI()
+from datetime import datetime
 
 if __name__ == "__main__":
-    main()
+    try:
+        text_ui = UI.UI("Model/model.pk")
+        text_ui.run()
+    except:
+        pass
+    
+
+    
