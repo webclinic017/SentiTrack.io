20045d3ff38994339a814df9f7e680515cc6428a
Parth Patel <parthpatel@parths-mbp.myfiosgateway.com>
2021-06-12 16:06:34 -0400

    Change sentiment score calculation

--- src/Classifier.py
+++ src/Classifier.py
@@ -1,28 +1,42 @@
 import pickle
 from CustomTransformer import TextCleaner, cleanText
+import datetime
 
 class Classifier :
     
     def __init__(self, model_path):
         with open(model_path, 'rb') as file:
             self.model = pickle.load(file)
-
-    def getSentiment(self, comments):
+    
+    def __getScore(self, predictions):
 
         """
-        Returns pct bullishness in batch of comments passed in
+        Returns sentiment score (between -1 to 1) for the interval.
         """
+        score = 0
+        comments = 0
+        for prediction in predictions:
+            if prediction == "Bull":
+                score += 1
+                comments += 1
+            elif prediction == "Bear":
+                score -= 1
+                comments +=1
+        return round(score/comments, 2)
 
-        predictions = self.model.predict(comments)
-        bull = 0
-        bear = 0
-        neutral = 0
-        for p in predictions:
-            if p == "Bear":
-                bear += 1
-            elif p == "Bull":
-                bull += 1
-            else:
-                neutral += 1
-        bullishness = round((bull / (bull + bear)), 2)
-        return bullishness
+
+    def getSentiment(self, range_map):
+
+        """
+        Returns sentiment score for each interval in range map
+        """
+        data = {}
+        for interval in range_map:
+            predictions = self.model.predict(interval[1])
+            score = self.__getScore(predictions)
+            data[datetime.datetime.strftime(interval[0][1], '%H:%M:%S')] = score
+        return data
+
+
+        
+
