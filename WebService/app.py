fff9ee91d5feb2392ced0281a3cdf37c0c3c414d
Parth Patel <parthpatel@parths-mbp.myfiosgateway.com>
2021-06-28 21:56:02 -0400

    Add end points for data and returns/performance

---
+++ WebService/app.py
@@ -0,0 +1,64 @@
+from flask import Flask
+from flask import render_template
+import os
+import json
+from config import db
+import pytz
+import pandas as pd
+import plotly.graph_objects as go
+
+app = Flask(__name__, template_folder='templates')
+
+def getReturn(row):
+    # if negative sentiment, change negative, profit the negative 
+    # if positive sentiment, change positive, profit the positive
+    # if negative sentiment, change positive, lose the positive
+    # if positive sentiment, change negative, lose the negative
+
+    if row['Sentiment'] < 0 and row['pct_chg'] < 0:
+        return row['pct_chg']
+    elif row['Sentiment'] > 0 and row['pct_chg'] > 0:
+        return row['pct_chg']
+    elif row['Sentiment'] < 0 and row['pct_chg'] > 0:
+        return -1 * row['pct_chg']
+    elif row['Sentiment'] > 0 and row['pct_chg'] < 0:
+        return row['pct_chg']
+    else:
+        return 1
+
+@app.route("/performance")
+def getPerformance():
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM WSB')
+    table_rows = cursor.fetchall()
+    df = pd.DataFrame(table_rows)
+    df.columns = ['Time', 'Sentiment', 'Market']
+
+    #compute pct accuracy
+    df['pct_chg'] = df['Market'].pct_change().shift(-1)
+    correct = len(df[((df['pct_chg'] > 0) & (df['Sentiment'] > 0)) | ((df['pct_chg'] < 0) & (df['Sentiment'] < 0))])
+    total = len(df[df['pct_chg'] != 0])
+    pct_correct = round(correct/total, 2)
+
+    #compute all time returns 
+    df['pct_chg'] = df['pct_chg'] + 1
+    returns = round(df['pct_chg'].prod(),3)
+    return json.dumps({"accuracy": pct_correct, "returns": returns})
+
+
+@app.route("/data")
+def getData():
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM WSB')
+    table_rows = cursor.fetchall()
+    df = pd.DataFrame(table_rows)
+    df.columns = ['Time', 'Sentiment', 'Market']
+    eastern = pytz.timezone('US/Eastern')
+    df['Time'] = df['Time'].dt.tz_localize(pytz.utc).dt.tz_convert(eastern).astype(str)
+    #df.set_index('Time', inplace=True)
+    result = df.to_dict(orient='list')
+    return json.dumps(result)
+
+@app.route("/")
+def index():
+    return render_template('main.html')
